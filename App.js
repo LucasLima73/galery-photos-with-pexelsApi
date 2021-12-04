0dc937c0294c6141072b6aee788434c4a9a632ad
Lucas <lucaslavey@outlook.com>
2021-12-04 11:03:02 -0300

    create app

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from "react-native";
 
-export default function App() {
+const { width, height } = Dimensions.get("screen");
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
+const IMAGE_SIZE = 80;
+
+const fetchImagesFromPexels = async (page) => {
+  const data = await fetch(API_URL, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+
+  const { photos } = await data.json();
+  return photos;
+  console.log(result);
+};
+
+export default () => {
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    const fetchImages = async () => {
+      const images = await fetchImagesFromPexels();
+      setImages(images);
+    };
+    fetchImages();
+  }, []);
+
+  const topRef = useRef();
+  const thumbRef = useRef();
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const scrollToActiveIndex = (index) => {
+    setActiveIndex(index);
+    topRef?.current?.scrollToOffset({
+      offset: index * width,
+      animated: true,
+    });
+    if (index * (IMAGE_SIZE + 10) - IMAGE_SIZE / 2 > width / 2) {
+      thumbRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + 10) - width + IMAGE_SIZE / 2,
+        animated: true,
+      });
+    } else {
+      thumbRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + 10) - IMAGE_SIZE / 2,
+        animated: true,
+      });
+    }
+  };
+
+  if (!images) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 50, fontWeight: "bold"}}>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <FlatList
+        data={images}
+        ref={topRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        onMomentumScrollEnd={(event) => {
+          scrollToActiveIndex(
+            Math.floor(event.nativeEvent.contentOffset.x / width)
+          );
+        }}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, height }}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={[StyleSheet.absoluteFillObject]}
+              />
+            </View>
+          );
+        }}
+      />
+      <FlatList
+        data={images}
+        ref={thumbRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ position: "absolute", bottom: IMAGE_SIZE }}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ paddingHorizontal: 5 }}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity onPress={() => scrollToActiveIndex(index)}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={{
+                  width: IMAGE_SIZE,
+                  height: IMAGE_SIZE,
+                  borderRadius: 12,
+                  marginRight: 10,
+                  borderWidth: 2,
+                  borderColor: activeIndex === index ? "#fff" : "transparent",
+                }}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
